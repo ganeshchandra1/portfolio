08ca2db1ef10a993ab8f166f072ab469d4b6afbe
Ganesh Chandra <cganesh208@gmail.com>
2021-02-03 11:04:00 +0530

    made navbar  responsive

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const togglebutton=document.getElementsByClassName('toggle-button')[0];
 const navbarContents=document.getElementsByClassName('nav-contents')[0];
+const navli=document.querySelectorAll('li')
+
 togglebutton.addEventListener('click',()=>{
     navbarContents.classList.toggle('active')
+    
 })
+navli.forEach(nav=>{
+    nav.addEventListener('click',()=>{
+    navbarContents.classList.toggle('active') 
+})
+})
+
